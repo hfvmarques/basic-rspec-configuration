ef70e8314a56d7e211683a4f95b874624c7a727c
Henrique <hfvmarques@gmail.com>
2022-03-30 13:28:23 -0400

    fixtures

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  fixtures :customers
+
+  it 'creates a customer' do
+    # customer = Customer.create(name: 'John Doe', email: 'example@example.com')
+    # subject.name = 'John Doe'
+    # subject.email = 'john@doe.com'
+    # subject.save
+
+    customer = customers(:john)
+
+    expect(customer.name).to eq('John Doe')
+  end
 end
