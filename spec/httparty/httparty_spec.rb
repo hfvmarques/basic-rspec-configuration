4cdc8139cf2a481bd02051f7eb6aab64ad3c0778
hfvmarques <hfvmarques@gmail.com>
2022-04-13 15:47:15 -0400

    webmock

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,5 +1,7 @@
 describe "HTTParty" do
-  it 'HTTParty.get' do
+  it 'content_type' do
+    stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").to_return(status: 200, body: "", headers: { 'content-type': 'application/json' })
+
     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
     content_type = response.headers['content-type']
 
