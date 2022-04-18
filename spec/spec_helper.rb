4419644b5d80b165cd824c869fe74be1a27e8240
hfvmarques <hfvmarques@gmail.com>
2022-04-17 21:29:37 -0400

    random order spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,6 +30,8 @@ RSpec.configure do |config|
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
 
+  config.order = 'random'
+
   config.before(:suite) do
     FactoryBot.lint
   end
