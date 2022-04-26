080fb4add9fd7775fff9bca055aa348fd7303aba
hfvmarques <hfvmarques@gmail.com>
2022-04-26 09:35:00 -0400

    controller testing

---
+++ spec/controllers/customers_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe CustomersController, type: :controller do
+  it 'responds successfully' do
+    get :index
+    
+    expect(response).to be_successful
+  end
+
+  it 'gets a 200 response' do
+    get :index
+    
+    expect(response).to have_http_status(200)
+  end
+end
