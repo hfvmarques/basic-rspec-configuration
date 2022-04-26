0e2da70ff34679635fc758fe97f6a851d264f5d6
hfvmarques <hfvmarques@gmail.com>
2022-04-26 11:27:45 -0400

    testing controllers with user entry

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -38,5 +38,20 @@ RSpec.describe CustomersController, type: :controller do
       it { expect(response).to have_http_status(200) }
       it { expect(response).to render_template(:show) }
     end
+    
+    context 'POST #create' do
+      before do
+        member = create(:member)
+        sign_in member
+
+        customer = create(:customer)
+      end
+
+      it 'with valid attributes' do
+        customer_params = attributes_for(:customer)        
+
+        expect { post :create, params: { customer: customer_params } }.to change(Customer, :count).by(1)
+      end
+    end
   end
 end
