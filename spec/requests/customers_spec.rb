6a9f6d5797412a2e70ad6e74648cce587695d652
hfvmarques <hfvmarques@gmail.com>
2022-05-02 21:12:52 -0400

    rspec matchers for json responses

--- spec/requests/customers_spec.rb
+++ spec/requests/customers_spec.rb
@@ -20,9 +20,22 @@ RSpec.describe 'Customers', type: :request do
       
       expect(response).to have_http_status(200)
       expect(response.body).to include_json([
-        name: "Shera Sherail",
-        email: "shera@sherail.com"      
+        id: /\d/,
+        name: (be_kind_of String),
+        email: (be_kind_of String)      
       ])
+    end
+
+    it 'gets a customer as JSON' do
+      customer = create(:customer)
+      get "/customers/#{customer.id}.json"
+      
+      expect(response).to have_http_status(200)
+      expect(response.body).to include_json(
+        id: /\d/,
+        name: (be_kind_of String),
+        email: (be_kind_of String)      
+      )
     end
   end
 end
